859e83a10b94540d38727494fe42b2882072d60a
Bilal Karakira <Bkarakira@terra.net.lb>
2025-09-30 13:53:21 +0300

    new version

--- logger/logging.py
+++ logger/logging.py
@@ -3,18 +3,12 @@ import sys
 from typing import Optional
 
 def get_logger(name: Optional[str] = None, level: int = logging.INFO) -> logging.Logger:
-      """
-    Create and return a logger with the given name.
-
-    Args:
-        name (str): Name of the logger (usually your app name or module).
-        level (int): Logging level. Defaults to INFO.
-    """
-      logger = logging.getLogger(name)
-      logger.setLevel(level)
-      handler = logging.StreamHandler(sys.stdout)
-      handler.setLevel(level)
-      formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-      handler.setFormatter(formatter)
-      logger.addHandler(handler)
-      return logger
+  logger = logging.getLogger(name)
+  logger.setLevel(level)
+  handler = logging.StreamHandler(sys.stdout)
+  handler.setLevel(level)
+  formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  handler.setFormatter(formatter)
+  logger.addHandler(handler)
+  
+  return logger
