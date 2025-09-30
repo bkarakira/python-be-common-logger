c3440313b04b5d04a2331c7b1bf3e2d250fcb906
Bilal Karakira <Bkarakira@terra.net.lb>
2025-09-30 14:05:32 +0300

    Logger fix

--- logger/logging.py
+++ logger/logging.py
@@ -5,10 +5,15 @@ from typing import Optional
 def get_logger(name: Optional[str] = None, level: int = logging.INFO) -> logging.Logger:
   logger = logging.getLogger(name)
   logger.setLevel(level)
+  
   handler = logging.StreamHandler(sys.stdout)
   handler.setLevel(level)
+
   formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+  
   handler.setFormatter(formatter)
   logger.addHandler(handler)
   
+  print("version 2")
+
   return logger
